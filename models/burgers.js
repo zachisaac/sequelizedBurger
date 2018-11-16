46daca570c8d97b0a7313f59a142b7ebefb67637
zpaki5142 <zachisaac@users.noreply.github.com>
2018-11-15 23:37:14 -0800

    first commit

---
+++ models/burgers.js
@@ -0,0 +1,27 @@
+
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+      burger_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      devoured: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      }
+    });
+  
+  Burger.associate = function(models) {
+  
+      Burger.belongsTo(models.Customer, {
+        foreignKey: {
+        }
+      });
+  };
+  
+    return Burger;
+  };
